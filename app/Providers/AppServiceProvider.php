578b4699e5537fa261c27087721cf510e6fefca6
Argi <argiigmg@gmail.com>
2026-05-14 13:40:22 +0700

    feat: implement rate limiting for post and interaction actions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('post-limit', function ($request) {
+            return Limit::perMinute(2)->response(function () {
+                return response()->json([
+                    'status'  => 429,
+                    'message' => 'Ups! Kamu sering melakukan aksi, coba tunggu sebentar ya.'
+                ], 429);
+            });
+        });
+
+        RateLimiter::for('interaction-limit', function ($request) {
+            return Limit::perMinute(60)->response(function () {
+                return response()->json([
+                    'status'  => 429,
+                    'message' => 'Ups! Kamu sering melakukan aksi, coba tunggu sebentar ya.'
+                ], 429);
+            });
+        });
+
+        RateLimiter::for('posts-limit', function ($request) {
+            return Limit::perMinute(60)->response(function () {
+                return response()->json([
+                    'status'  => 429,
+                    'message' => 'Ups! Kamu sering melakukan aksi, coba tunggu sebentar ya.'
+                ], 429);
+            });
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,32 +8,44 @@ use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 
-Route::get("/user", function (Request $request) {
-    return $request->user();
-})->middleware("auth:sanctum");
 
-// get User
-Route::get('/user/{id}', [ProfileController::class, 'getUser']);
+Route::get("/", function () {
+    return "Hello from wepost!";
+})->middleware(['throttle:post-limit']);
 
-Route::post("/login", [AuthController::class, "login"]);
-Route::post("/register", [AuthController::class, "register"]);
-Route::get("/posts", [PostController::class, "index"]);
-Route::get("/post/{id}", [PostController::class, "show"]);
+Route::middleware(['throttle:auth-limit'])->group(function () {
+    Route::post("/login", [AuthController::class, "login"]);
+    Route::post("/register", [AuthController::class, "register"]);
+});
+
+Route::middleware(['throttle:posts-limit'])->group(function () {
+    // get User
+    Route::get('/user/{id}', [ProfileController::class, 'getUser']);
+
+    // get Posts
+    Route::get("/posts", [PostController::class, "index"]);
+    Route::get("/post/{id}", [PostController::class, "show"]);
+});
 
 Route::middleware("auth:sanctum")->group(function () {
     Route::get("/me", [AuthController::class, "me"]);
     Route::post("/logout", [AuthController::class, "logout"]);
 
     // Posts
-    Route::post("/post", [PostController::class, "store"]);
-    Route::put("/post/{id}", [PostController::class, "update"]);
-    Route::delete("/post/{id}", [PostController::class, "delete"]);
+    Route::middleware(['throttle:post-limit'])->group(function () {
+        Route::post("/post", [PostController::class, "store"]);
+        Route::put("/post/{id}", [PostController::class, "update"]);
+        Route::delete("/post/{id}", [PostController::class, "delete"]);
+    });
 
-    // likes
+    // Activity
     Route::get("/like-activity", [LikeController::class, "likeActivity"]);
-    Route::post("/post/{id}/like", [LikeController::class, "like"]);
-
-    // Favorit
     Route::get("/favorits-activity", [FavoritController::class, 'ActivityFavorit']);
-    Route::post("/post/{id}/favorit", [FavoritController::class, 'Favorit']);
+
+    Route::middleware(['throttle:interaction-limit'])->group(function () {
+        // likes
+        Route::post("/post/{id}/like", [LikeController::class, "like"]);
+        // Favorit
+        Route::post("/post/{id}/favorit", [FavoritController::class, 'Favorit']);
+    });
 });
